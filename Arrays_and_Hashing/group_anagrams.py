4d6d38229955f296f4e15de19ffcf65280870f72
Michelle Oh <ohmotrl@gmail.com>
2023-05-27 23:50:11 -0400

    initialized group anagrams

---
+++ Arrays_and_Hashing/group_anagrams.py
@@ -0,0 +1,31 @@
+'''GROUP ANAGRAMS
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.'''
+
+def group_anagrams(strs) -> list:
+    pass
+
+print(group_anagrams(["eat","tea","tan","ate","nat","bat"]))
+print(group_anagrams([""]))
+print(group_anagrams(["a"]))
