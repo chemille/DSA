2ae53fe6906c2f4b86739b2272cc8cfb2d405476
chemille <ohmotrl@gmail.com>
2025-02-20 19:40:36 -0500

    Added another binary search solution

--- 5_binary_search/2_Search_a_2D_Matrix-m.py
+++ 5_binary_search/2_Search_a_2D_Matrix-m.py
@@ -39,25 +39,46 @@ n == matrix[i].length
 
 def searchMatrix(matrix, target):
         ## My first solution ##
-        ## Time complexity should be O(m*n), since m is the number of rows and n is the number of columns in the matrix
-        ## Basically visiting each element of the matrix once
+        ## This might be O(log(m*n)) ##
     
-        l_row = 0
-        r_row = len(matrix) - 1
+        # l_row = 0
+        # r_row = len(matrix) - 1
 
-        for row in range(len(matrix)):
-            m_row = l_row + (r_row - l_row) // 2
-            for item in matrix[m_row]:
-                if target == item:
-                    return True
-                elif target > matrix[m_row][-1]:
-                    l_row = m_row + 1
-                elif target < matrix[m_row][0]:
-                    r_row = m_row - 1
+        # for row in range(len(matrix)):
+        #     m_row = l_row + (r_row - l_row) // 2
+        #     for item in matrix[m_row]:
+        #         if target == item:
+        #             return True
+        #         elif target > matrix[m_row][-1]:
+        #             l_row = m_row + 1
+        #         elif target < matrix[m_row][0]:
+        #             r_row = m_row - 1
 
-        else:
-            return False
+        # else:
+        #     return False
    
+    ## Copilot suggestions for binary search implementation ##
+    ## Time complexity is O(log(m*n)), since m is the number of rows and n is the number of columns in the matrix
+    ## This implementation uses a single binary search over the entire matrix, treating it as a flattened array, which ensures the time complexity is O(log(m * n)). ##
+    
+    if not matrix or not matrix[0]:
+        return False
 
+    m, n = len(matrix), len(matrix[0])
+    left, right = 0, m * n - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        mid_value = matrix[mid // n][mid % n]
+
+        if mid_value == target:
+            return True
+        elif mid_value < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
+    
 print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)) # True
 print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)) # False
